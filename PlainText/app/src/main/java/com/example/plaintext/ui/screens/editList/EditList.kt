e3bf9152661e37f4776f6914f9565b694b39d39b
ThiagoChaves517 <zuruarque@hotmail.com>
2026-06-10 01:19:10 -0400

    Implementing skeleton of EditList screen

--- PlainText/app/src/main/java/com/example/plaintext/ui/screens/editList/EditList.kt
+++ PlainText/app/src/main/java/com/example/plaintext/ui/screens/editList/EditList.kt
@@ -1,23 +1,22 @@
 package com.example.plaintext.ui.screens.editList
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import com.example.plaintext.R
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
@@ -27,12 +26,15 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.plaintext.data.model.PasswordInfo
 import com.example.plaintext.ui.screens.Screen
 import com.example.plaintext.ui.screens.login.TopBarComponent
+import java.nio.file.WatchEvent
+
 
 data class EditListState(
     val nomeState: MutableState<String>,
@@ -51,9 +53,42 @@ fun EditList(
     navigateBack: () -> Unit,
     savePassword: (password: PasswordInfo) -> Unit
 ) {
-
+    Scaffold(
+        topBar = { TopBarComponent() },
+        content = { innerPadding ->
+            EditListContent(innerPadding)
+        }
+    )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditListContent(scaffoldInnerPadding: PaddingValues) {
+    Column(
+        modifier = Modifier
+            .padding(scaffoldInnerPadding)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Column(
+            modifier = Modifier
+                .background(colorResource(R.color.teal_700))
+        ) {
+            TopAppBar(
+                title = { Text("Adicionar nova senha") }
+            )
+        }
+
+        EditInput(stringResource(R.string.input_name))
+        EditInput(stringResource(R.string.input_user))
+        EditInput(stringResource(R.string.input_password))
+        EditInput(stringResource(R.string.input_notes))
+        Button(onClick = {}) {
+            Text(text = "Salvar")
+        }
+    }
+}
 
 @Composable
 fun EditInput(
